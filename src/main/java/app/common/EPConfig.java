52643056a6fe1dbb1f554f9d8e34d001a834dc1d
e <1131374063@qq.com>
2023-01-12 16:45:52 +0800

    Initial commit

---
+++ src/main/java/app/common/EPConfig.java
@@ -0,0 +1,19 @@
+package app.common;
+
+public class EPConfig {
+     //mysql
+   /*  public static final String mysql_host="10.22.1.70";
+     public static final int mysql_port=3306;
+     public static final String mysql_database="easy_project22";
+     public static final String mysql_user="sunwei";
+     public static final String mysql_password="Lyric@sunwei";
+     public static final String mysql_timezone="Asia/Shanghai";*/
+
+
+     public static final String mysql_host="hadoop01";
+     public static final int mysql_port=3306;
+     public static final String mysql_database="BlueIot";
+     public static final String mysql_user="root";
+     public static final String mysql_password="123456";
+     public static final String mysql_timezone="Asia/Shanghai";
+}
---
+++ src/main/java/app/ods/ODSProject.java
@@ -0,0 +1,34 @@
+package app.ods;
+
+
+import app.common.EPConfig;
+import app.func.CustomerDeserialization;
+import com.ververica.cdc.connectors.mysql.MySqlSource;
+import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.debezium.DebeziumSourceFunction;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class ODSProject {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DebeziumSourceFunction<String> mySqlData = MySqlSource.<String>builder()
+                .hostname(EPConfig.mysql_host)
+                .port(EPConfig.mysql_port)
+                .username(EPConfig.mysql_user)
+                .password(EPConfig.mysql_password)
+                .databaseList(EPConfig.mysql_database)
+                .tableList("BlueIot.tb_inspect")
+                .serverTimeZone(EPConfig.mysql_timezone)
+                .deserializer(new CustomerDeserialization())
+                .startupOptions(StartupOptions.initial())
+                .build();
+
+        DataStreamSource<String> streamSource = env.addSource(mySqlData);
+
+        streamSource.print();
+        env.execute();
+
+    }
+}
